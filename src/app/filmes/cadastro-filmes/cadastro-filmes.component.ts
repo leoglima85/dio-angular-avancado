a6cb15c8fa0e8f65bfb0fdf6c788aceff27f09d5
leoglima <leoglima@gmail.com>
2021-09-17 14:33:30 -0300

    array dinamico no select - ok

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -11,6 +11,7 @@ import { ValidarCamposService } from 'src/app/shared/components/campos/validar-c
 export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
+  generos: Array<string>;
 
   constructor(public validacao: ValidarCamposService, private fb: FormBuilder) { }
 
@@ -29,6 +30,8 @@ export class CadastroFilmesComponent implements OnInit {
       urlIMDB: ['', [Validators.minLength(10)]],
       genero: ['',[Validators.required]]
     });
+
+    this.generos = ['Ação','Aventura','Comedia','Terror','Ficção Cientifica','Comedia','Drama'];
 
   }
 
--- src/app/shared/components/campos/input-select/input-select.component.ts
+++ src/app/shared/components/campos/input-select/input-select.component.ts
@@ -12,6 +12,8 @@ export class InputSelectComponent  {
   @Input () titulo: string;
   @Input () formGroup: FormGroup;
   @Input () controlName: string; 
+  @Input () opcoes : Array<string>;
+
   constructor(public validacao: ValidarCamposService) { }
 
   get formControl (): AbstractControl {
