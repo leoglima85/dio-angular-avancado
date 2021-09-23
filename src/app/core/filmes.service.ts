7813ce4471ba211cbb2f33f349f96fbf14d33eb9
leoglima <leoglima@gmail.com>
2021-09-23 14:41:22 -0300

    excluir filmes - ok

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -26,4 +26,12 @@ export class FilmesService {
   visualizar (id : number): Observable<Filme> {
     return this.http.get<Filme>(url + id);
   }
+
+  excluir (id: number): Observable<void> {
+    return this.http.delete<void>(url + id);
+  }
+
+  editar(filme: Filme): Observable<Filme> {
+    return this.http.put<Filme>(url + filme.id, filme);
+  }
 }
--- src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
+++ src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
+import { Alerta } from 'src/app/shared/models/alerta';
+import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 
 @Component({
   selector: 'dio-visualizar-filmes',
@@ -11,15 +14,43 @@ import { Filme } from 'src/app/shared/models/filme';
 export class VisualizarFilmesComponent implements OnInit {
   readonly semfoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg';
   filme: Filme;
-  constructor(private activatedRoute: ActivatedRoute,
+  id: number;
+
+  constructor(public dialog: MatDialog,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
               private filmesService: FilmesService) { }
 
   ngOnInit() {
-    this.visualizar(this.activatedRoute.snapshot.params['id']);
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.visualizar();
   }
 
-  private visualizar (id: number) : void {
-    this.filmesService.visualizar(id).subscribe((filme: Filme)=>this.filme = filme);
+  editar(): void {
+    this.router.navigateByUrl('/filmes/cadastro/' + this.id);
+  }
+
+  excluir(): void {
+    const config = {
+      data: {
+        titulo: 'Você tem certeza que deseja excluir?',
+        descricao: 'Caso você tenha certceza que deseja excluir, clique no botão OK',
+        corBtnCancelar: 'primary',
+        corBtnSucesso: 'warn',
+        possuirBtnFechar: true
+      } as Alerta
+    };
+    const dialogRef = this.dialog.open(AlertaComponent, config);
+    dialogRef.afterClosed().subscribe((opcao: boolean) => {
+      if (opcao) {
+        this.filmesService.excluir(this.id)
+        .subscribe(() => this.router.navigateByUrl('/filmes'));
+      }
+    });
+  }
+
+  private visualizar(): void {
+    this.filmesService.visualizar(this.id).subscribe((filme: Filme) => this.filme = filme);
   }
 
 }
